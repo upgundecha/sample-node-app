ff9a4d10debad68a0dfe3f1488b9947a9d97d041
Unmesh Gundecha <upgundecha@gmail.com>
2018-12-05 23:57:37 +0800

    Added form

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('It works!');
+    res.render('form',  { title: 'Registration Form' });
 });
 
 module.exports = router;
