398378f194b4d4bca5f76480af8f8b9d611e6d3a
Unmesh Gundecha <upgundecha@gmail.com>
2018-12-06 23:19:11 +0800

    Added validation

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator/check');
 
 router.get('/', (req, res) => {
     res.render('form',  { title: 'Registration Form' });
 });
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.render('form',  { title: 'Registration Form' });
+router.post('/', 
+    [
+        body('name')
+            .isLength({min: 1})
+            .withMessage('Please enter a name'),
+        body('email')
+            .isLength({min: 1})
+            .withMessage('Please enter an email')
+     ],(req, res) => {
+        const errors = validationResult(req);
+
+        if(errors.isEmpty()) {
+            res.send('Thank you for your registration!');
+        } else {
+            res.render('form', {
+                title: 'Registration Form',
+                errors: errors.array(),
+                data: req.body,
+            });
+        }
 });
 
 module.exports = router;
