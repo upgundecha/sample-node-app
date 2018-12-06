d21e1cce858bf88b26aadf7e98defdacce4f20b8
Unmesh Gundecha <upgundecha@gmail.com>
2018-12-06 23:07:58 +0800

    Added body parser

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,9 @@ router.get('/', (req, res) => {
     res.render('form',  { title: 'Registration Form' });
 });
 
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.render('form',  { title: 'Registration Form' });
+});
+
 module.exports = router;
