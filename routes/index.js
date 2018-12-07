247790e9076697a9da333062bc2ee077a093af3e
Unmesh Gundecha <upgundecha@gmail.com>
2018-12-07 23:29:34 +0800

    Added listing page

--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,21 @@ const Registration = mongoose.model('Registration');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('form',  { title: 'Registration Form' });
+    res.render('form', { title: 'Registration Form' });
 });
 
-router.post('/', 
+router.post('/',
     [
         body('name')
-            .isLength({min: 1})
+            .isLength({ min: 1 })
             .withMessage('Please enter a name'),
         body('email')
-            .isLength({min: 1})
+            .isLength({ min: 1 })
             .withMessage('Please enter an email')
-     ],(req, res) => {
+    ], (req, res) => {
         const errors = validationResult(req);
 
-        if(errors.isEmpty()) {
+        if (errors.isEmpty()) {
             const registration = new Registration(req.body);
             registration.save()
                 .then(() => { res.send('Thank you for your registration!'); })
@@ -32,6 +32,15 @@ router.post('/',
                 data: req.body,
             });
         }
+    });
+
+router.get('/registrations', (req, res) => {
+    Registration.find()
+        .then((registrations) => {
+            res.render('index', { title: 'Listing registrations', registrations })
+        })
+        .catch(() => { res.send('Sorry! Something went wrong.'); })
+    res.render('index', { title: 'Listing registrations' });
 });
 
 module.exports = router;
